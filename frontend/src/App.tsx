98d8c672e76497edf1320a533c215ae80ba7dc97
Filiberto Rios <filibertoerios@gmail.com>
2023-10-09 12:43:43 -0500

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <header>
+        Sunflower Candies
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        All Rights Reserved
+      </footer>
+    </div>
+  )
+}
+
+export default App
